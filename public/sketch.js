2d99632abb0a0d5eb3a6a85ea2d35e8b94ecfccc
Gyuhyeon_Lee <mark3236@gmail.com>
2016-10-17 20:36:41 +0900

    ...relative link?

--- public/sketch.js
+++ public/sketch.js
@@ -19,7 +19,7 @@ function setup(){
 		video[i].loop();
 		video[i].hide();
 		*/
-		video[i] = loadImage("crowdvoteapp.com/stream1");
+		video[i] = loadImage("http://crowdvoteapp.com/stream1");
 	}
 	//video = loadImage(['http://217.7.233.140/cgi-bin/faststream.jpg?stream=full&fps=0']);
 	//video.loop();
